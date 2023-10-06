53d1c0e55092bed785401afd52fda1c311b54dcd
Michael Ndula <michaelndula@gmail.com>
2023-10-06 09:47:23 +0300

    added the sudan metadata

---
+++ ihs-sudan-metadata/ihs_queries/reporting_age.sql
@@ -0,0 +1,39 @@
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order, sex) values('<1 yr M','Less than 1 year', 0, 0,1, 0,0, 'M');
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order, sex) values('<1 yr F','Less than 1 year', 0, 0,1, 0,0, 'F');
+
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order, sex) values('1-4 yr M','1 to 4 years', 1, 0,4, 0,0, 'M');
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order, sex) values('1-4 yr F','1 to 4 years', 1, 0,4, 0,0, 'F');
+
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order,  sex) values('5-9 yr M','5 to 9 years', 5, 0,9, 0,0, 'M');
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order,  sex) values('5-9 yr F','5 to 9 years', 5, 0,9, 0,0, 'F');
+
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order,  sex) values('10-14 yr M','10 to 14 years', 10, 0,14, 0,0, 'M');
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order,  sex) values('10-14 yr F','10 to 14 years', 10, 0,14, 0,0, 'F');
+
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order,  sex) values('15-19 yr M','15 to 19 years', 15, 0,19, 0,0, 'M');
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order,  sex) values('15-19 yr F','15 to 19 years', 15, 0,19, 0,0, 'F');
+
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order,  sex) values('20-24 yr M','20 to 24 years', 20, 0,24, 0,0, 'M');
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order,  sex) values('20-24 yr F','20 to 24 years', 20, 0,24, 0,0, 'F');
+
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order,  sex) values('25-29 yr M','25 to 29 years', 25, 0,29, 0,0, 'M');
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order,  sex) values('25-29 yr F','25 to 29 years', 25, 0,29, 0,0, 'F');
+
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order,  sex) values('30-34 yr M','30 to 34 years', 30, 0,34, 0,0, 'M');
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order,  sex) values('30-34 yr F','30 to 34 years', 30, 0,34, 0,0, 'F');
+
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order,  sex) values('35-39 yr M','35 to 39 years', 35, 0,39, 0,0, 'M');
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order,  sex) values('35-39 yr F','35 to 39 years', 35, 0,39, 0,0, 'F');
+
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order,  sex) values('40-44 yr M','40 to 44 years', 40, 0,44, 0,0, 'M');
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order,  sex) values('40-44 yr F','40 to 44 years', 40, 0,44, 0,0, 'F');
+
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order,  sex) values('45-49 yr M','45 to 49 years', 45, 0,49, 0,0, 'M');
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order,  sex) values('45-49 yr M','45 to 49 years', 45, 0,49, 0,0, 'F');
+
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order,  sex) values('50+ yr M','50and above years', 50, 0,200, 0,0, 'M');
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order,  sex) values('50+ yr F','50and above years', 50, 0,200, 0,0, 'F');
+
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order,  sex) values('Total','Total M ', 0, 0,200, 0,0, 'M');
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order,  sex) values('Total','Total F ', 0, 0,200, 0,0, 'F');
+Insert into reporting_age_group(name,report_group_name, min_years, min_days, max_years,max_days, sort_order,  sex) values('Total','Total All', 0, 0,200, 0,0, 'M');
